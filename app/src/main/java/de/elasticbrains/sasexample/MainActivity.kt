d02dca5dac5fad058d2d97a0896321c9fae95f0b
Drilon Blakqori <drilon.blakqori@elasticbrains.de>
2019-09-02 13:27:30 +0200

    Cleanup

--- app/src/main/java/de/elasticbrains/sasexample/MainActivity.kt
+++ app/src/main/java/de/elasticbrains/sasexample/MainActivity.kt
@@ -3,16 +3,12 @@ package de.elasticbrains.sasexample
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import com.smartadserver.android.library.model.SASAdElement
-import com.smartadserver.android.library.util.SASConfiguration
 import com.smartadserver.android.library.model.SASAdPlacement
-import com.smartadserver.android.library.ui.SASBannerView
+import com.smartadserver.android.library.util.SASConfiguration
 import kotlinx.android.synthetic.main.activity_main.*
-import java.lang.Exception
 
 
-class MainActivity: Activity (), SASBannerView.BannerListener {
+class MainActivity: Activity () {
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +16,6 @@ class MainActivity: Activity (), SASBannerView.BannerListener {
         SASConfiguration.getSharedInstance().configure(this, 311478, "https://mobile.smartadserver.com");
 
         val adPlacement = SASAdPlacement(311578, "1123762", 73927, null)
-        bannerView.bannerListener = this
         bannerView.loadAd(adPlacement)
 
         theButton.setOnClickListener {
@@ -28,35 +23,4 @@ class MainActivity: Activity (), SASBannerView.BannerListener {
         }
     }
 
-    override fun onBannerAdClicked(bannerView: SASBannerView?) {
-        Log.i("MainActivity","onBannerAdClicked $bannerView")
-    }
-
-    override fun onBannerAdLoaded(bannerView: SASBannerView?, adElement: SASAdElement?) {
-        Log.i("MainActivity","onBannerAdLoaded")
-    }
-
-    override fun onBannerAdCollapsed(bannerView: SASBannerView?) {
-        Log.i("MainActivity","onBannerAdCollapsed")
-    }
-
-    override fun onBannerAdClosed(bannerView: SASBannerView?) {
-        Log.i("MainActivity","onBannerAdClosed")
-    }
-
-    override fun onBannerAdResized(bannerView: SASBannerView?) {
-        Log.i("MainActivity","onBannerAdResized")
-    }
-
-    override fun onBannerAdExpanded(bannerView: SASBannerView?) {
-        Log.i("MainActivity","onBannerAdExpanded")
-    }
-
-    override fun onBannerAdVideoEvent(bannerView: SASBannerView?, id: Int) {
-        Log.i("MainActivity","onBannerAdVideoEvent")
-    }
-
-    override fun onBannerAdFailedToLoad(bannerView: SASBannerView?, exception: Exception?) {
-        Log.i("MainActivity","onBannerAdFailedToLoad $exception")
-    }
 }
